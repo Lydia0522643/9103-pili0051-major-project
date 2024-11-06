953a3f8a7f6120f35cf5ed001c3217b52b97e9fd
Lydia0522643 <pili0051@uni.sydney.edu.au>
2024-11-06 23:16:24 +1100

    Add circles and DotCircle
    Added a regularly spaced centre circle, a dotted circle around the centre of the circle, and two zigzag lines radiating around the centre of the circle.

--- sketch.js
+++ sketch.js
@@ -1,11 +1,51 @@
-let width = 500;
-let height = 500;
 
 function setup() {
-  createCanvas(width, height);
-  background(6, 110, 153); // Setting the blue background
+  createCanvas(500, 500);
+  background(6, 110, 153);
 
-  // Data on points
+  
+  // Creating a circle with radial lines
+  let radialCircle1 = new RadialLinesCircle(215, 30,135/2, 35, '#bfe0f0', 35); // Radius of 35 vacated in the centre
+  let radialCircle2 = new RadialLinesCircle(140, 330,135/2, 35, '#bfe0f0', 35);
+  radialCircle1.draw();
+  radialCircle2.draw();
+
+
+  // circles
+  let circle1 = new ConcentricCircles(65, 70, 135, 80, 5, '#272b5f', '#bfe0f0');
+  let circle2 = new ConcentricCircles(175, 180, 135, 80, 5, '#159439', '#e3f2ef');
+  let circle3 = new ConcentricCircles(290, 290, 135, 80, 5, '#dd3d4f', '#fff6fa');
+  let circle4 = new ConcentricCircles(400, 400, 135, 80, 5, '#15438a', '#F4C54C');
+  let circle5 = new ConcentricCircles(510, 510, 135, 80, 5, '#774E41', 'orange');
+  
+  let circle6 = new ConcentricCircles(330, 140, 135, 80, 5, '#c093c3', '#fab632');
+  let circle7 = new ConcentricCircles(440, 250, 135, 80, 5, '#f07e35', '#f2f8f6');
+  
+  let circle8 = new ConcentricCircles(370, -10, 135, 80, 5, '#e4271e', '#fceae6');
+  let circle9 = new ConcentricCircles(480, 100, 135, 80, 5, '#22315b', '#f6af4f');
+  
+  let circle10 = new ConcentricCircles(20, 220, 135, 80, 5, '#004985', '#f5b423');
+  let circle11 = new ConcentricCircles(250, 440, 135, 80, 5, '#cd1a52', '#f8b66a');
+  
+  let circle12 = new ConcentricCircles(-10, 370, 135, 80, 5, '#009d90', '#daeef3');
+  let circle13 = new ConcentricCircles(100, 480, 135, 80, 5, '#e52929', '#fdfdfd');
+
+  // Draw the circle
+  circle1.draw();
+  circle2.draw();
+  circle3.draw();
+  circle4.draw();
+  circle5.draw();
+  circle6.draw();
+  circle7.draw();
+  circle8.draw();
+  circle9.draw();
+  circle10.draw();
+  circle11.draw();
+  circle12.draw();
+  circle13.draw();
+
+  // Punti per il secondo codice
   const points = [
     [4, 136], [95, 157], [137, 96], [136, 4], [97, 243], [242, 118],
     [264, 202], [305, 55], [356, 224], [201, 264], [53, 305], [75, 396],
@@ -15,15 +55,15 @@ function setup() {
     [0, 28], [25, -10], [6, 458], [40, 540], [300, 500]
   ];
 
-  // Connect all points with distance <= 108 and generate a random ellipse.
-
+  // Connetti punti con distanza <= 108
   connectPoints(points, 108);
   
-  // Generate random ellipses at each point
+  // Genera ellissi casuali nei punti
   generateRandomEllipses(points);
 }
 
-// Function to connect points, only points with distance <= maxDistance are connected.
+// Funzioni ausiliarie
+
 function connectPoints(points, maxDistance) {
   points.forEach((start, i) => {
     points.forEach((end, j) => {
@@ -37,7 +77,6 @@ function connectPoints(points, maxDistance) {
   });
 }
 
-// Drawing Connection Lines and Ellipses
 function drawConnection(start, end, distance) {
   const angle = calculateAngle(...start, ...end);
   let radiusX = (distance - 12) / 6;
@@ -52,27 +91,24 @@ function drawConnection(start, end, distance) {
   pop();
 }
 
-// Generate random ellipse
 function generateRandomEllipses(points) {
   for (let i = 0; i < points.length; i++) {
     let x = points[i][0];
     let y = points[i][1];
 
-    let w = random(15, 20);  
-    let h = random(15, 20);  
-    let angle = random(-PI / 9, PI / 9);  
+    let w = random(15, 20);
+    let h = random(15, 20);
+    let angle = random(-PI / 9, PI / 9);
 
     push();
     translate(x, y);
     rotate(angle);
 
-     
-    stroke(232, 120, 15);  
+    stroke(232, 120, 15);
     strokeWeight(3);
-    fill(0);  
+    fill(0);
     ellipse(0, 0, w, h);
 
-     
     noStroke();
     fill(255);
     ellipse(0, 0, w / 3, h / 3);
@@ -81,14 +117,12 @@ function generateRandomEllipses(points) {
   }
 }
 
-// Calculated Distance
 function calculateDistance(x1, y1, x2, y2) {
   const deltaX = x2 - x1;
   const deltaY = y2 - y1;
-  return Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
+  return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
 }
 
-// Calculate the angle
 function calculateAngle(x1, y1, x2, y2) {
   const deltaY = y2 - y1;
   const deltaX = x2 - x1;
@@ -96,32 +130,118 @@ function calculateAngle(x1, y1, x2, y2) {
   return angleRadians * (180 / Math.PI);
 }
 
-// Creating random coloured ellipses
 
-function createEllipse(inputXPos, inputYPos, inputRadiusX, inputRadiusY, inputR, inputG, inputB) {
-  let outputXPos = inputXPos;
-  let outputYPos = inputYPos;
-  let outputRadiusX = inputRadiusX;
-  let outputRadiusY = inputRadiusY;
-  let outputColour = makeRGB(inputR, inputG, inputB); 
-
-  fill(outputColour);
+function createEllipse(xPos, yPos, radiusX, radiusY) {
+  fill(random(255), random(255), random(255));
   stroke('orange');
   strokeWeight(2);
-  
-  // Drawing Ellipses
-  ellipse(outputXPos, outputYPos, outputRadiusX * 2, outputRadiusY * 2);
+  ellipse(xPos, yPos, radiusX * 2, radiusY * 2);
 }
 
-// Generate random RGB colours
-function makeRGB(redInputValue, greenInputValue, blueInputValue) {
-  let redOutputValue = redInputValue ?? randomRoundedValue(255);
-  let greenOutputValue = greenInputValue ?? randomRoundedValue(255);
-  let blueOutputValue = blueInputValue ?? randomRoundedValue(255);
-  return color(redOutputValue, greenOutputValue, blueOutputValue);
+// class for circles
+class ConcentricCircles {
+  constructor(x, y, outerDiameter, innerDiameter, numCircles, dotColor, fillColor) {
+    this.x = x;
+    this.y = y;
+    this.outerDiameter = outerDiameter;
+    this.innerDiameter = innerDiameter;
+    this.numCircles = numCircles;
+    this.dotColor = dotColor;
+    this.fillColor = fillColor;
+    this.diameterStep = (outerDiameter - innerDiameter) / (numCircles - 1);
+  }
+
+  draw() {
+    // Disegna il cerchio centrale con riempimento
+    noStroke();
+    fill(this.fillColor);
+    circle(this.x, this.y, 140);
+    
+    // Disegna i cerchi concentrici
+    for (let i = 0; i < this.numCircles; i++) {
+      let currentDiameter = this.outerDiameter - i * this.diameterStep;
+      this.drawDashedCircle(this.x, this.y, currentDiameter, 6, this.dotColor, 2); 
+    }
+  }
+
+  drawDashedCircle(x, y, diameter, dotSize, lineColor, spacing) {
+    stroke(lineColor);
+    noFill();
+
+    let radius = diameter / 2;
+    let circumference = TWO_PI * radius;
+    let numDots = floor(circumference / (dotSize + spacing));
+
+    for (let i = 0; i < numDots; i++) {
+      let angle = map(i, 0, numDots, 0, TWO_PI);
+      let xDot = x + radius * cos(angle);
+      let yDot = y + radius * sin(angle);
+
+      fill(lineColor);
+      noStroke();
+      circle(xDot, yDot, dotSize);
+    }
+  }
 }
 
 
-function randomRoundedValue(maxValue) {
-  return Math.round(random(0, maxValue));
+
+class RadialLinesCircle {
+  constructor(x, y, outerRadius, numLines, color, innerRadius) {
+    this.x = x;
+    this.y = y;
+    this.outerRadius = outerRadius;
+    this.numLines = numLines;
+    this.color = color;
+    this.innerRadius = innerRadius; // Radius of the vacated area in the centre
+  }
+
+  draw() {
+    // Drawing an external circle
+    fill(this.color);
+    noStroke();
+    ellipse(this.x, this.y, this.outerRadius * 2);
+    
+      // Calculate the angle increment for each point
+    let angleStep = TWO_PI / this.numLines;
+    
+     // Store all outer and inner points
+    let outerPoints = [];
+    let innerPoints = [];
+    
+    
+    // Drawing Radial Lines
+    stroke('#ec5800'); // line colour
+    strokeWeight(3);
+    for (let i = 0; i < this.numLines; i++) {
+      let angle1 = map(i, 0, this.numLines, 0, TWO_PI);
+      let x1 = this.x + cos(angle1) * this.outerRadius;
+      let y1 = this.y + sin(angle1) * this.outerRadius;
+      line(this.x, this.y, x1, y1);
+      
+        let angle2 = i * angleStep;
+      let xOuter = this.x + cos(angle2) * this.outerRadius;
+      let yOuter = this.y + sin(angle2) * this.outerRadius;
+      let xInner = this.x + cos(angle2) * this.innerRadius;
+      let yInner = this.y + sin(angle2) * this.innerRadius;
+      
+      outerPoints.push({x: xOuter, y: yOuter});
+      innerPoints.push({x: xInner, y: yInner});
+    }
+    
+     // Drawing Radial Zigzag Lines
+    stroke('#ec5800');
+    strokeWeight(3);
+    for (let i = 0; i < this.numLines; i++) {
+      let nextIndex = (i + 1) % this.numLines;  // Guaranteed Loop Connection
+
+      // Connect the current outer point to the next inner point.
+      line(outerPoints[i].x, outerPoints[i].y, innerPoints[nextIndex].x, innerPoints[nextIndex].y);
+    }
+
+    // Draw a blank area in the centre
+    fill(6, 110, 153); // background colour
+    noStroke();
+    ellipse(this.x, this.y, this.innerRadius * 2); // Empty area in the centre
+  }
 }
